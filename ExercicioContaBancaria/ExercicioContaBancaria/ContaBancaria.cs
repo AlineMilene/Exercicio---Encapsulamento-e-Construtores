17ed38c56a2b75aee5969693bd528cf2a66ff64e
Aline Milene <alinem2104@gmail.com>
2021-07-02 00:55:16 -0300

    Exercicio de conta bancaria em C# com aplicacao de encapsulamento e construtores

---
+++ ExercicioContaBancaria/ExercicioContaBancaria/ContaBancaria.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+namespace ExercicioContaBancaria {
+    class ContaBancaria {
+
+        // declaração dos atributos 
+        private string _nomeTitular;
+        public int Numero { get; private set; }
+        public double Saldo { get; private set; }
+
+        // criação dos construtores
+
+        public ContaBancaria(int numero, string nome) {
+            _nomeTitular = nome;
+            Numero = numero;
+        }
+        public ContaBancaria(int numero, string nome, double depositoInicial) : this(numero, nome) {
+            Deposito(depositoInicial);
+        }
+
+        // método de proteção para mudanças no Nome
+        public string Nome {
+            get { return _nomeTitular; }
+            set {
+                if (value != null && value.Length > 1) {
+                    _nomeTitular = value;
+                }
+            }
+        }
+
+        // outros métodos
+        public void Saque(double valor) {
+            Saldo -= valor + 5.00;
+        }
+        public void Deposito(double valor) {
+            Saldo += valor;
+        }
+        public override string ToString() {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Nome
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExercicioContaBancaria/ExercicioContaBancaria/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioContaBancaria {
+    class Program {
+        static void Main(string[] args) {
+            // criando a conta
+            ContaBancaria conta;
+
+            // perguntas iniciais
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+
+            // validação de deposito inicial
+            if (resp == 's' || resp == 'S') {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, nome, depInicial);
+
+                Console.WriteLine();
+                Console.WriteLine("Dados da conta:");
+                Console.WriteLine(conta);
+            }
+            else {
+                conta = new ContaBancaria(numero, nome);
+
+                Console.WriteLine();
+                Console.WriteLine("Dados da conta:");
+                Console.WriteLine(conta);
+            }
+
+            // perguntas finais
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(valor);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(valor);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
